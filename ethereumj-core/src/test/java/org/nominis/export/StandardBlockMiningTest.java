5240dc6000048c93115bb9c761d3a55412a84f0b
hhicks13 <hhicks13@vols.utk.edu>
2018-01-01 17:13:18 -0500

    "Dec 31, created readFromJSONfile test. Still under construction"

--- ethereumj-core/src/test/java/org/nominis/export/StandardBlockMiningTest.java
+++ ethereumj-core/src/test/java/org/nominis/export/StandardBlockMiningTest.java
@@ -1,8 +1,6 @@
 package org.nominis.export;
 
-import org.ethereum.core.Account;
-import org.ethereum.core.Genesis;
-import org.ethereum.core.Transaction;
+import org.ethereum.core.*;
 import org.ethereum.core.genesis.GenesisLoader;
 import org.ethereum.util.blockchain.StandaloneBlockchain;
 import org.json.simple.JSONObject;
@@ -12,9 +10,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Random;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.spongycastle.util.encoders.Hex;
+
 
 import static org.ethereum.util.FastByteComparisons.equal;
 import static org.ethereum.util.blockchain.EtherUtil.Unit.ETHER;
@@ -32,15 +36,24 @@ import static org.junit.Assert.assertTrue;
  **/
 
 public class StandardBlockMiningTest {
+        // fields used together with @Parameter must be public
 
-    //WARNING YOU WILL WANT TO ALTER THIS STRING FOR YOUR LOCAL MACHINE !!!
-    public static String PATH = "/Users/harrisonhicks/Documents/github_nominis/ethereumj/ethereumj-core/src/test/resources/genesis/allocated-genesis.json";
-    public StandaloneBlockchain sb;
-    public ArrayList<Account> accounts;
-    public ArrayList<Transaction> transactions;
-    public static Genesis genesis;
-    public final int numAccounts = 5;
-    public long[] initialBalances;
+        private BlockchainImpl blockchain;
+
+        private String PATH = "/Users/harrisonhicks/Documents/github_nominis/ethereumj/ethereumj-core/src/test/resources/genesis/allocated-genesis.json";
+
+        private StandaloneBlockchain sb;
+
+        private ArrayList<Account> accounts;
+
+        private ArrayList<Transaction> transactions;
+
+        private Genesis genesis;
+
+        private final int numAccounts = 5;
+
+        private long[] initialBalances;
+
     /**
      * Tests accountGenerator to create a list of accounts, and writes to json file.
      */
@@ -58,7 +71,7 @@ public class StandardBlockMiningTest {
         JSONObject balance;
         long amount;
         initialBalances = new long[numAccounts];
-        for(int i =0;i < accounts.size();i++) {
+        for (int i = 0; i < accounts.size(); i++) {
             balance = new JSONObject();
 
             //store initial amounts in array for later reference
@@ -66,32 +79,39 @@ public class StandardBlockMiningTest {
             initialBalances[i] = amount;
 
             //convert amount into weis and encode as JSON object
-            balance.put("balance",convert(amount, ETHER).toString());
-            addresses.put(Hex.toHexString(accounts.get(i).getAddress()),balance);
+            balance.put("balance", convert(amount, ETHER).toString());
+            addresses.put(Hex.toHexString(accounts.get(i).getAddress()), balance);
 
         }
 
-        genesisJSon.put("alloc",addresses);
-        genesisJSon.put("nonce","0x0000000000000000");
-        genesisJSon.put("difficulty","0x10");
-        genesisJSon.put("mixhash","0x0000000000000000000000000000000000000000000000000000000000000000");
-        genesisJSon.put("coinbase","0x0000000000000000000000000000000000000000");
-        genesisJSon.put("timestamp","0x00");
-        genesisJSon.put("parentHash","0x0000000000000000000000000000000000000000000000000000000000000000");
-        genesisJSon.put("extraData","0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa");
-        genesisJSon.put("gasLimit","0x1000000000");
+        genesisJSon.put("alloc", addresses);
+        genesisJSon.put("nonce", "0x0000000000000000");
+        genesisJSon.put("difficulty", "0x10");
+        genesisJSon.put("mixhash", "0x0000000000000000000000000000000000000000000000000000000000000000");
+        genesisJSon.put("coinbase", "0x0000000000000000000000000000000000000000");
+        genesisJSon.put("timestamp", "0x00");
+        genesisJSon.put("parentHash", "0x0000000000000000000000000000000000000000000000000000000000000000");
+        genesisJSon.put("extraData", "0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa");
+        genesisJSon.put("gasLimit", "0x1000000000");
 
         // try-with-resources statement based on post comment below :)
-        try (FileWriter file = new FileWriter(PATH,false)) {
+        try (FileWriter file = new FileWriter(PATH, false)) {
             file.write(genesisJSon.toJSONString());
             System.out.println("Successfully Copied JSON Object to File...");
             System.out.println("\nJSON Object: " + genesisJSon);
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Java IO exception caught when trying to write to file");
         }
 
+
+        /**
+         * This section of the test utilizes genesis loader class in an attempt to read from the genesis file created previously
+         */
+    }
+
+    @Test
+    public void readFromGenesis(){
+        Random rand = new Random();
         genesis = GenesisLoader.loadGenesis(
                 getClass().getResourceAsStream("/genesis/allocated-genesis.json"));
         final StandaloneBlockchain bc = new StandaloneBlockchain();
@@ -112,24 +132,20 @@ public class StandardBlockMiningTest {
         assertEquals(BigInteger.valueOf(actualBalance), retrievedBalance);
     }
 
-    @Test
-    public void standAloneBlockchainReadFromGenesis(){
 
-    }
-
-    @Test
-    public void multipleTransactions(){
-//        transactions = createTransactions.getTransactions(accounts,10);
+//    @Test
+//    public void multipleTransactions(){
+////        transactions = createTransactions.getTransactions(accounts,10);
+////
+////        int counter = 0;
+////        for(Transaction tRandom : transactions){
+////            System.out.println("Transaction "+counter);
+////            System.out.println(tRandom.toString());
+////            System.out.println("Raw transaction "+counter+" data: 0x{}" + org.apache.commons.codec.binary.Hex.encodeHexString(tRandom.getEncodedRaw()));
+////            counter++;
+////        }
 //
-//        int counter = 0;
-//        for(Transaction tRandom : transactions){
-//            System.out.println("Transaction "+counter);
-//            System.out.println(tRandom.toString());
-//            System.out.println("Raw transaction "+counter+" data: 0x{}" + org.apache.commons.codec.binary.Hex.encodeHexString(tRandom.getEncodedRaw()));
-//            counter++;
-//        }
-
-    }
+//    }
 
 
 }
